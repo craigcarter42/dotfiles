363ff96e5fed5bcc9cef17ab00926fd5f8102862
Craig Carter <32251992+craigcarter42@users.noreply.github.com>
2020-01-28 00:56:25 -0800

    Create profileutil.sh

---
+++ profileutil.sh
@@ -0,0 +1,31 @@
+#   _____            __ _ _        _    _ _   _ _ 
+#  |  __ \          / _(_) |      | |  | | | (_) |
+#  | |__) | __ ___ | |_ _| | ___  | |  | | |_ _| |
+#  |  ___/ '__/ _ \|  _| | |/ _ \ | |  | | __| | |
+#  | |   | | | (_) | | | | |  __/ | |__| | |_| | |
+#  |_|   |_|  \___/|_| |_|_|\___|  \____/ \__|_|_|
+
+#! /usr/bin/env bash
+
+if [ -e ~/.bash_profie ]; then
+	prof='~/.bash_profile'
+else
+	echo " [ERROR] No profile was found --EXIT"
+fi
+
+
+
+# Setup profile-util
+alias profile="cat ~/.profile_util/profile-about.txt"
+alias profile--reload="source ~/$prof"
+alias profile--view="cat ~/$prof"
+alias profile--edit="vim ~/$prof"
+alias profile--info="uname -a; uptime"
+alias profile--default="sh /Users/craigcarter/Profile/.dotfiles/.profile-blank.sh"
+alias profile--reset="source ~/.bash_profile"
+
+# Get config files
+source "/Users/admin/Profile/.dotfiles/.alias"
+source "/Users/admin/Profile/.dotfiles/.inputrc"
+source "/Users/admin/Profile/.dotfiles/.functions"
+
